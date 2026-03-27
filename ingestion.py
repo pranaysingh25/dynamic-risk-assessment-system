60990b453e55237eac44f40fc5560b556122c3d8
pranaysingh25 <pranaysingh.cs@gmail.com>
2026-03-26 20:04:47 -0400

    add code for data ingestion

--- ingestion.py
+++ ingestion.py
@@ -19,6 +19,30 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
+    csv_files = sorted(
+        [file for file in os.listdir(input_folder_path) if file.endswith(".csv")]
+    )
+
+    dataframes = []
+    for file in csv_files:
+        file_path = os.path.join(input_folder_path, file)
+        dataframes.append(pd.read_csv(file_path))
+
+    if dataframes:
+        final_df = pd.concat(dataframes, ignore_index=True).drop_duplicates()
+    else:
+        final_df = pd.DataFrame()
+
+    os.makedirs(output_folder_path, exist_ok=True)
+
+    final_data_path = os.path.join(output_folder_path, "finaldata.csv")
+    final_df.to_csv(final_data_path, index=False)
+
+    ingested_files_path = os.path.join(output_folder_path, "ingestedfiles.txt")
+    with open(ingested_files_path, "w") as f:
+        f.write(str(csv_files))
+
+    return final_df
 
 
 
